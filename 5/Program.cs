3f3d897b388d7bc8f6b9616bf7e598c0bc054a03
dudolad_yaroslav <dudolad2009@gmail.com>
2025-11-22 18:51:09 +0200

    created figure 5

---
+++ 5/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 7;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    bool top = (j >= i && j < n - i);
+                    bool bottom = (j >= n - i - 1 && j <= i);
+
+                    if (top || bottom)
+                        Console.Write(" *");
+                    else
+                        Console.Write("  ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
