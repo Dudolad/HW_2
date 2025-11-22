d157d1646739c236e368791a712d6fb340919db0
dudolad_yaroslav <dudolad2009@gmail.com>
2025-11-22 18:18:34 +0200

    Add project files.

---
+++ HW_2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace HW_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 8; 
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (j >= i)
+                        Console.Write(" *");
+                    else
+                        Console.Write("  ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
