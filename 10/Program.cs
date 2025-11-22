7f1f5fa427b48931db7c34f1cd47c74be25d384b
dudolad_yaroslav <dudolad2009@gmail.com>
2025-11-22 19:04:07 +0200

    created triangle 10

--- 10/Program.cs
+++ 10/Program.cs
@@ -1,22 +1,23 @@
-﻿namespace _9
+﻿namespace _10
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int n = 8;
+            int n = 7;
 
             for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < n; j++)
                 {
-                    if (j <= n - i - 1)
+                    if (j >= n - i - 1)
                         Console.Write(" *");
                     else
                         Console.Write("  ");
                 }
+
                 Console.WriteLine();
             }
-        }  
+        }   
     }
 }
