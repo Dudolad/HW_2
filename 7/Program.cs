25117f095e68ba8bcc79b83f3cb6985829fa72df
dudolad_yaroslav <dudolad2009@gmail.com>
2025-11-22 18:54:38 +0200

    created triangle 7

---
+++ 7/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 7;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (j <= i && j < n - i)
+                        Console.Write(" *");
+                    else
+                        Console.Write("  ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
