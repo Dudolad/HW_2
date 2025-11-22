7a3402b5cc70d113dfe2c1c26e0723e87c2ce7ec
dudolad_yaroslav <dudolad2009@gmail.com>
2025-11-22 18:52:33 +0200

    created figure 6

---
+++ 6/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 7;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    bool left = (j <= i && j < n - i);
+
+                    bool right = (j >= n - i - 1 && j >= i);
+
+                    if (left || right)
+                        Console.Write(" *");
+                    else
+                        Console.Write("  ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
